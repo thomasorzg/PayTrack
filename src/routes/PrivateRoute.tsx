ad1972d241bd42fc195468752d3ba5e616527b19
thomasorzg <jaapagamerz12@gmail.com>
2024-03-10 23:35:39 -0700

    Actualizar package.json y PrivateRoute.tsx

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const { isAuthenticated } = useAuth();
 
-    useEffect(() => {
-        const isLogged = async () => {
-            const token = localStorage.getItem("authToken");
-            if (token === null) {
-                setIsAuthenticated(false);
-                return;
-            }
-
-            // Hacer la petición al servidor para verificar si el token es válido
-            /* const response = await fetch("http://localhost:4000/auth/verify", {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-
-            setIsAuthenticated(response.ok);*/
-
-            // Por ahora, asumimos que el usuario está autenticado si hay un token
-            setIsAuthenticated(true);
-        };
-
-        isLogged();
-    }, []);
-
-    return isAuthenticated ? <>{children}</> : <Navigate to="/dashboard" replace />;
+    return isAuthenticated ? <>{children}</> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
